56458ed11519b4c59caeef321fd4fdb8e99c98d7
Harsh <harshgoyal@gmail.com>
2016-04-20 21:00:21 -0500

    kkkk

--- main.cpp
+++ main.cpp
@@ -19,10 +19,19 @@ CutNode generateCut(double a, double b, double c, double d)
 }
 
 
-int main(int argc, char *argv[])
-{
-  CutNode cn = generateCut(1,1,1,1);
-  cout << cn.n << "\n" << cn.pt << "\n";
-  cout << slice(&cn, "Colonel.obj") << "\n";
-  return 0;
+int main(int argc, char *argv[]) {
+  cout << "im here\n";
+
+  double z = 0;
+  for(double x = -1; x <= 1; x += 0.2) {
+    for(double y = -(sqrt(1 - x*x)); y <= sqrt(1 - x*x); y += 0.2) {
+      for(double l = 0; l < 110; l += 10) {
+        cout << "in loop\n";
+        z = 1 - x*x - y*y;
+        CutNode cn = generateCut(x, y, z, l);
+        cout << " x= " <<  x << " y = " << y << " z = " << z << " l = " << l << " cost = " << slice(&cn, "Colonel.obj") << "\n";
+      }
+    }
+  }
 }
+
