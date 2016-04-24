82a2b99b406502dc7aff87d74904c9317158825e
Souvik Banerjee <souvikb@cs.utexas.edu>
2016-04-24 18:38:20 -0500

    Rewrite rbf.py

---
+++ rbf2.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import scipy
+from scipy.interpolate import Rbf
+import sys
+import math
+import numpy as np
+a = []
+b = []
+c = []
+d = []
+cost = []
+with open(sys.argv[1]) as fn:
+    for line in fn:
+        cols = line.split()
+        if len(cols) > 4 and np.random.randint(0,1) == 0:
+            a.append(float(cols[0]))
+            b.append(float(cols[1]))
+            c.append(math.sqrt(abs(float(cols[2]))))
+            d.append(float(cols[3]))
+            cost.append(float(cols[4]))
+rbfi = Rbf(a, b, c, d, cost, epsilon=0.01, smooth=10)
+ctr = 0
+for ctr in xrange(0, 901):
+    print((a[ctr], b[ctr], c[ctr], d[ctr], rbfi(a[ctr], b[ctr], c[ctr], d[ctr]), cost[ctr]))
